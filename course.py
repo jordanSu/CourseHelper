5f6c3817f36c890004349bdefcc53a8b1667f7cc
jordanSu <newjordansu1126@gmail.com>
2016-02-28 21:43:29 +0800

    add Python3.x support

--- course.py
+++ course.py
@@ -6,21 +6,25 @@ import re
 import sys
 if sys.version_info < (3,2):
     import urllib
+    isPython3 = False
 else:
     import urllib.request as urllib
+    isPython3 = True
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        try: input = raw_input
-        except NameError: pass
+        if isPython3 is not True:
+            input = raw_input
         ans = input("系所代碼: ")
     else:
-        ans = sys.argv[1]
+        ans = "F7" #sys.argv[1]
 
     url = "http://course-query.acad.ncku.edu.tw/qry/qry001.php?dept_no=" + ans
 
     try:
         data = urllib.urlopen(url).read()   # Parse the raw data
+        if isPython3 is True:
+            data = data.decode()
     except IOError:
         print("連線錯誤!!!")
         exit()
@@ -28,12 +32,7 @@ if __name__ == "__main__":
     if "查無課程資訊" in data:    # maybe the DeptNo. is wrong
         print("查無課程資訊，請檢查課程代號是否正確！")
     else:
-        print(type(data))
         content = re.findall("<TD(.+?)</TD>", data)
-        """
-        for i in range(0, len(content)):
-            print i, content[i]
-        """
         maxLength = 0
         nameList = []
         remainList = []
@@ -44,18 +43,28 @@ if __name__ == "__main__":
                 thisRemain = re.findall('.+>(.+)', content[i+9])[0]
                 remainList.append(thisRemain)
 
-                if len(thisName.decode("utf8")) > maxLength:
-                    maxLength = len(thisName.decode("utf8"))
-        print(maxLength)
+                # To find the maxLength
+                if isPython3 is True:
+                    if len(thisName) > maxLength:
+                        maxLength = len(thisName)
+                else:
+                    if len(thisName.decode("utf8")) > maxLength:
+                        maxLength = len(thisName.decode("utf8"))
+
         for j in range(0, len(nameList)):   #To print every course in the list
             space = []
             engCharSub = re.findall('[a-zA-Z]', nameList[j])
-
-            for k in range(0, maxLength+2 - len(nameList[j].decode("utf8"))):
-                space.append('  ')
             for l in range(0, len(engCharSub)):
                 space.append(' ')
-            space = "".join(space)
 
-            print(nameList[j].decode("utf8"), space, remainList[j].decode("utf8"))
+            if isPython3 is True:
+                for k in range(0, maxLength+2 - len(nameList[j])):
+                    space.append('  ')
+                space = "".join(space)
+                print(nameList[j], space, remainList[j])
+            else:
+                for k in range(0, maxLength+2 - len(nameList[j].decode("utf8"))):
+                    space.append('  ')
+                space = "".join(space)
+                print(nameList[j].decode("utf8"), space, remainList[j].decode("utf8"))      
             print("-----------------------------------------------")
